52ff30b531bdc847ab8910f051c29b3ccac72bd9
Yanbo Cheng <86802756+iamyanbo@users.noreply.github.com>
2023-12-05 17:16:46 -0500

    sh files

---
+++ startup.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+# If this script gives "-bash: ./startup.sh: /bin/bash^M: bad interpreter: No such file or directory", run dos2unix startup.sh
+sudo apt update
+sudo apt install -y python3-pip
+sudo apt install python3-virtualenv
+virtualenv venv
+source venv/bin/activate
+sudo apt-get install libtiff5-dev libjpeg8-dev libopenjp2-7-dev zlib1g-dev \
+    libfreetype6-dev liblcms2-dev libwebp-dev tcl8.6-dev tk8.6-dev python3-tk \
+    libharfbuzz-dev libfribidi-dev libxcb1-dev
+pip3 install -r requirements.txt
+
+rm */migrations/0*.py
+rm db.sqlite3
+python3 manage.py makemigrations
+python3 manage.py migrate
+python3 script.py
